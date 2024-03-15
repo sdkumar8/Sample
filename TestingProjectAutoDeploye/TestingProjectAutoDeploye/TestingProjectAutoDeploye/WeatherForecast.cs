f4a5498e6917e02a911e2f92dd863db834891bc6
sdkumar8 <162314783+sdkumar8@users.noreply.github.com>
2024-03-15 14:52:02 +0530

    test WeatherForecast.cs

--- TestingProjectAutoDeploye/TestingProjectAutoDeploye/TestingProjectAutoDeploye/WeatherForecast.cs
+++ TestingProjectAutoDeploye/TestingProjectAutoDeploye/TestingProjectAutoDeploye/WeatherForecast.cs
@@ -9,6 +9,5 @@ namespace TestingProjectAutoDeploye
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
-        public string? Summarys { get; set; }
     }
 }
